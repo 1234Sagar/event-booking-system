22e7c9cfcb492853891d6d9aecf82232330bae10
1234Sagar <129657266+1234Sagar@users.noreply.github.com>
2024-10-16 13:47:27 +0530

    Initial commit

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const SearchBar = ({ setSearchTerm, setCategory }) => {
+  return (
+    <div className="mb-4 flex space-x-4">
+      <input
+        type="text"
+        placeholder="Search events..."
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="px-4 py-2 border rounded-md flex-grow"
+      />
+      <select
+        onChange={(e) => setCategory(e.target.value)}
+        className="px-4 py-2 border rounded-md"
+      >
+        <option value="">All Categories</option>
+        <option value="Music">Music</option>
+        <option value="Technology">Technology</option>
+        <option value="Food">Food</option>
+        <option value="Film">Film</option>
+        
+      </select>
+    </div>
+  );
+};
+
+export default SearchBar;
